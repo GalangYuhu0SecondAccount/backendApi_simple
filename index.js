a108c6d9609a6baecf202bbd12a34d42ec5400fe
GalangYuhu <galangmrd09@gmail.com>
2024-05-20 06:22:05 +0700

    UP

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
   });
-app.use(helmet());
+// app.use(helmet());
 app.use(morgan("combined"));
 
 const authenticateApiKey = require("./Auth");
