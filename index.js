e0917667ac7c9847f87fe7da65d319e70bb77ac9
GalangYuhu <galangmrd09@gmail.com>
2024-05-20 06:14:44 +0700

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
-const helmet = require("helmet");
+// const helmet = require("helmet");
 
 const app = express();
 
