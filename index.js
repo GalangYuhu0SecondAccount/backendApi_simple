01cf0ff378f4008da1a1a817a54bc9f1cc97460e
GalangYuhu <galangmrd09@gmail.com>
2024-05-20 06:13:35 +0700

    UP

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(bodyParser.json())
 app.use(cors({
     origin : "*"
 }));
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });
 app.use(helmet());
 app.use(morgan("combined"));
 
