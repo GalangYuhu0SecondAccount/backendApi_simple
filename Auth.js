9e39abf1a5e4ec50797f1a876f9a83a67b023889
GalangYuhu <galangmrd09@gmail.com>
2024-05-20 11:50:06 +0700

    UP

--- Auth.js
+++ Auth.js
@@ -5,7 +5,7 @@ dotenv.config()
 function AuthenticationRoute (req , res , next) {
     const api_Key = req.headers["x-api-key"];
 
-    if (api_Key && api_Key === process.env.API_KEY) {
+    if (api_Key && "/users" === process.env.API_KEY) {
         next();
     } else {
         return res.status(400).json({message : "your have request api key"})
